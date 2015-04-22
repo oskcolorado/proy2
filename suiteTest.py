6b4448d783f257525881d37d90803e8b14a09d18
Luis <oskcolorado@gmail.com>
2015-04-22 09:57:45 -0430

    Creacion de suiteTest

---
+++ suiteTest.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-. 
+'''
+Created on 22/04/2015
+
+@author: Luis Colorado   09-11086
+         Jose Barrientos 10-10800
+'''
+
+import unittest
+from mdlaccesscontrol import clsAccessControl
